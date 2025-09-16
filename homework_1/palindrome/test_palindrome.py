7c7cd8d82842d89d5050ece540db2373ca2fd094
alekpak <alekpak@ozon.ru>
2025-09-16 16:47:55 +0300

    linters

--- homework_1/palindrome/test_palindrome.py
+++ homework_1/palindrome/test_palindrome.py
@@ -1,26 +1,25 @@
-import pytest
 from palindrome import is_palindrome
 
 
 def test_negative_value():
-    assert is_palindrome(-1) == False
+    assert not is_palindrome(-1)
 
 
 def test_zero():
-    assert is_palindrome(0) == True
+    assert is_palindrome(0)
 
 
 def test_digit():
-    assert is_palindrome(4) == True
+    assert is_palindrome(4)
 
 
 def test_not_palindrome():
-    assert is_palindrome(12345) == False
-    assert is_palindrome(10) == False
-    assert is_palindrome(1212) == False
+    assert not is_palindrome(12345)
+    assert not is_palindrome(10)
+    assert not is_palindrome(1212)
 
 
 def test_palindrome():
-    assert is_palindrome(1234321) == True
-    assert is_palindrome(1) == True
-    assert is_palindrome(909) == True
+    assert is_palindrome(1234321)
+    assert is_palindrome(1)
+    assert is_palindrome(909)
