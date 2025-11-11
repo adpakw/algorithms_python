7eed7174c803ba237e0d5f0d4e6d405dc164ffaa
adpak <adpakw@gmail.com>
2025-11-11 19:43:52 +0300

    hw6 compare

---
+++ homework_6/compare/sorters.py
@@ -0,0 +1,58 @@
+import time
+from typing import Any, Callable, List
+
+def timer(func: Callable) -> Callable:
+    def wrapper(*args, **kwargs) -> Any:
+        start_time = time.perf_counter()  # можно через time.process_time()
+        result = func(*args, **kwargs)
+        end_time = time.perf_counter()  # можно через time.process_time()
+        execution_time = end_time - start_time
+        print(f"Execution time of {func.__name__}: {execution_time:.4f} secs")
+        return result
+
+    return wrapper
+
+@timer
+def mergesort(arr: List[int]) -> List[int]:
+    def merge(left: List[int], right: List[int]) -> List[int]:
+        result = []
+        i = j = 0
+
+        while i < len(left) and j < len(right):
+            if left[i] <= right[j]:
+                result.append(left[i])
+                i += 1
+            else:
+                result.append(right[j])
+                j += 1
+
+        result.extend(left[i:])
+        result.extend(right[j:])
+        return result
+
+    def split(arr: List[int]) -> List[int]:
+        if len(arr) <= 1:
+            return arr
+
+        mid = len(arr) // 2
+        left = split(arr[:mid])
+        right = split(arr[mid:])
+
+        return merge(left, right)
+
+    return split(arr)
+
+@timer
+def quicksort(arr: List[int]) -> List[int]:
+    def recursive_quicksort(arr: List[int]) -> List[int]:
+        if len(arr) <= 1:
+            return arr
+
+        pivot = arr[0]  # можно брать первый/последний/по середине элемент
+        left = [x for x in arr if x < pivot]
+        middle = [x for x in arr if x == pivot]
+        right = [x for x in arr if x > pivot]
+
+        return recursive_quicksort(left) + middle + recursive_quicksort(right)
+
+    return recursive_quicksort(arr)
---
+++ homework_6/compare/test_compare.py
@@ -0,0 +1,57 @@
+import sys
+
+import numpy as np
+from sorters import mergesort, quicksort
+
+sys.setrecursionlimit(20_000)
+
+def test_random_list():
+    random_list = np.random.randint(
+        low=np.iinfo(np.int64).min, high=np.iinfo(np.int64).max, size=10_000_000
+    )
+
+    result_merge_sort_random_list = mergesort(random_list)
+    result_quick_sort_random_list = quicksort(random_list)
+
+    assert result_merge_sort_random_list == sorted(random_list)
+    assert result_quick_sort_random_list == sorted(random_list)
+
+
+def test_random_w_dublicates_list():
+    random_w_dublicates_list = np.random.randint(
+        low=-10, high=10, size=10_000_000
+    )
+
+    result_merge_sort_random_w_dublicates_list = mergesort(random_w_dublicates_list)
+    result_quick_sort_random_w_dublicates_list = quicksort(random_w_dublicates_list)
+
+    assert result_merge_sort_random_w_dublicates_list == sorted(random_w_dublicates_list)
+    assert result_quick_sort_random_w_dublicates_list == sorted(random_w_dublicates_list)
+
+def test_all_same_list():
+    all_same_list = [5] * 10_000_000
+
+    result_merge_sort_all_same_list = mergesort(all_same_list)
+    result_quick_sort_all_same_list = quicksort(all_same_list)
+    
+    assert result_merge_sort_all_same_list == sorted(all_same_list)
+    assert result_quick_sort_all_same_list == sorted(all_same_list)
+
+def test_sorted_list():
+    sorted_list = list(range(10_000))
+
+    result_merge_sort_sorted_list = mergesort(sorted_list)
+    result_quick_sort_sorted_list = quicksort(sorted_list)
+
+    assert result_merge_sort_sorted_list == sorted(sorted_list)
+    assert result_quick_sort_sorted_list == sorted(sorted_list)
+
+def test_sorted_list():
+    reversed_sorted = list(range(10_000, 0, -1))
+
+    result_merge_sort_reversed_sorted = mergesort(reversed_sorted)
+    result_quick_sort_reversed_sorted = quicksort(reversed_sorted)
+
+    assert result_merge_sort_reversed_sorted == sorted(reversed_sorted)
+    assert result_quick_sort_reversed_sorted == sorted(reversed_sorted)
+
